b9d9ab94b5d36bde51e8e17e2f19cdc3503eb67d
Malthe Petersen <malthe@maggerit.dk>
2017-01-31 09:47:13 +0100

    TIlføjet jQuery or style

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,9 @@
 <html>
 <head>
 	<title>something</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="content">
+	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<script src="js/jquery-3.1.1.min."></script>
 </head>
 <body>
