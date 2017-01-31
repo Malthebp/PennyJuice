d19ddb1ca09f96a3f5c4103ca3ffb3d7808f1c51
mathias <mathiasqvist@hotmail.com>
2017-01-31 12:34:57 +0100

    Tilføjet og færdiggjort about og contact

--- index.php
+++ index.php
@@ -1,24 +1,44 @@
 <?php 
 	include 'includes/header.php';
 ?>
-<section>
-
-    <h1 class="home">Penny Juice</h1>
-    
-    <img class="coin" src="image/coin.png">
-
-</section>
-
-<section id="about" class="container">
-
-</section>
-<section id="buy" class="container">
-</section>
-<section id="contact" class="container">
-</section>
-
-
-
-<?php 
+    <section>
+        <h1 class="home">Penny Juice</h1> <img class="coin" src="image/coin.png"> </section>
+    <section id="about" class="container">
+        <div class="about">
+            <article class="box">
+                <h3>Who is Penny Juice?</h3>
+                <p>Penny Juice is an American company, located in many different states. We specialize in juice for kids, and is one of the leading juice companies</p>
+            </article>
+            <article class="box">
+                <h3>What is Penny Juice?</h3>
+                <p>Penny Juice is a 100% blended fruit juice concentrate that is specifically designed for childcare centers, preschools, Head Starts, etc. PENNY JUICE uses only the highest quality fruit juice products available.</p>
+            </article>
+            <article class="box">
+                <h3>Reviews</h3>
+                <p>The children at kid college love the taste of Penny Juice, and drink it without encouragement from our teachers.</p>
+                <p>I like Penny Juice for that reason and because it is 100% juice from concentrate requiring no refrigeration prior to mixing. The handy mix pitcher take the guess work out of preparation</p>
+                <p><span>CASSIE PENCE - KID KOLLEGE/BILLINGS, MT</span></p>
+            </article>
+        </div>
+    </section>
+    <section id="buy" class="container"> </section>
+    <section id="contact" class="container">
+        <aside>
+            <p>Email:</p> <a href="pennyjuice@hotmail.com" <p>pennyjuice@hotmail.com</p>
+        </p></a>
+            <br>
+            <p>Tel:
+                <br> (763) 493-5023</p>
+            <br>
+            <p>Adress:</p>
+            <p>Davenport, IA 52807</p>
+        </aside>
+        <div class="googlemaps">
+            <?php
+        include 'includes/googlemaps.php';
+        ?>
+        </div>
+    </section>
+    <?php 
 	include 'includes/footer.php';
 ?>
