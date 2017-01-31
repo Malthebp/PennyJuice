b0ac03b1887ff441b70d6425f47e536aca558799
Malthe Petersen <malthe@maggerit.dk>
2017-01-31 09:58:47 +0100

    Tilføjet noget mere

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,5 @@
 
+<script src="js/jquery-3.1.1.min."></script>
+<script src="js/master.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,19 @@
 <html>
 <head>
-	<title>something</title>
+	<title>Penny Juice - Juiceboxes for kids</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="content">
+	<meta name="description" content="Get the best juice for children here. We know that you want to cheap, so our priceses matches your expectations.">
 	<link rel="stylesheet" type="text/css" href="style/style.css">
-	<script src="js/jquery-3.1.1.min."></script>
 </head>
 <body>
+
+
+	<header><!-- header -->
+		<nav> <!-- navigation -->
+			<ul>
+				<li><a class="active" href="#about" title="About Penny Juice">About</a></li>
+				<li><a href="#buy" title="Buy Juiceboxes at Penny Juice">Buy</a></li>
+				<li><a href="#contact" title="Contact Penny Juice">Contact</a></li>
+			</ul>
+		</nav> <!-- /navigation -->
+	</header><!-- /header -->
