572a70c57bc2f3246b75c69415e57c0cfde378b3
Malthe Petersen <malthe@maggerit.dk>
2017-01-31 10:07:30 +0100

    Tilføjet fonts

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 
-<script src="js/jquery-3.1.1.min."></script>
+<script src="js/jquery-3.1.1.min.js"></script>
 <script src="js/master.js"></script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="Get the best juice for children here. We know that you want to cheap, so our priceses matches your expectations.">
 	<link rel="stylesheet" type="text/css" href="style/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Dancing+Script|Lato" rel="stylesheet">
 </head>
 <body>
 
 
 	<header><!-- header -->
 		<nav> <!-- navigation -->
+
 			<ul>
-				<li><a class="active" href="#about" title="About Penny Juice">About</a></li>
+				<li><a class="#active" href="#about" title="About Penny Juice">About</a></li>
 				<li><a href="#buy" title="Buy Juiceboxes at Penny Juice">Buy</a></li>
 				<li><a href="#contact" title="Contact Penny Juice">Contact</a></li>
 			</ul>
